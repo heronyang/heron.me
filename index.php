eb8244086e744d27243e8cf8af14476d8cc7aee0
Heron Yang <heron.yang.tw@gmail.com>
2015-03-16 09:18:14 -0400

    add heroku needs stuffs

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE HTML>
+<!--
+	Astral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+
+  <title>Heron Yang</title>
+
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="description" content="Hi, I'm Heron Yang. I understand that, to reach my destination, I will have to keep continuing building up my skill sets with more experience, exchanging thoughts and ideas with those who either share similar dreams with me or have very different goals in life, and holding on what I have always believed in.">
+  <meta name="keywords" content="Heron Yang, NCTU, NCTUCS, NCTU CS" />
+  <meta name="author" content="Heron Yang" />
+  <meta name="copyright" content="Heron Yang" />
+  <meta name="viewport" content="width=device-width">
+
+  <!-- for Facebook -->
+  <meta property="og:title" content="Heron Yang" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="http://heron.me/images/me.jpg" />
+  <meta property="og:url" content="http://heron.me" />
+  <meta property="og:description" content="Hi, I'm Heron Yang. I understand that, to reach my destination, I will have to keep continuing building up my skill sets with more experience, exchanging thoughts and ideas with those who either share similar dreams with me or have very different goals in life, and holding on what I have always believed in." />
+
+  <!-- for Twitter -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:title" content="Heron Yang" />
+  <meta name="twitter:description" content="Hi, I'm Heron Yang. I understand that, to reach my destination, I will have to keep continuing building up my skill sets with more experience, exchanging thoughts and ideas with those who either share similar dreams with me or have very different goals in life, and holding on what I have always believed in." />
+  <meta name="twitter:image" content="http://heron.me/images/me.jpg" />
+
+  <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/skel.min.js"></script>
+  <script src="js/init.js"></script>
+  <noscript>
+    <link rel="stylesheet" href="css/skel.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/style-desktop.css" />
+    <link rel="stylesheet" href="css/style-noscript.css" />
+  </noscript>
+  <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+
+  <link rel="stylesheet" href="css/custom.css" />
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+</head>
+
+<body>
+
+  <!-- Wrapper-->
+  <div id="wrapper">
+
+    <!-- Nav -->
+    <nav id="nav">
+      <a href="#me" class="icon fa-home active"><span>Home</span></a>
+      <a href="#work" class="icon fa-folder"><span>Work</span></a>
+      <a href="resume.pdf" target="_blank" class="icon fa-file-text"><span>Resume</span></a>
+      <a href="#contact" class="icon fa-envelope"><span>Contact</span></a>
+    </nav>
+
+    <!-- Main -->
+    <div id="main">
+
+      <!-- Me -->
+      <article id="me" class="panel">
+        <header>
+          <h1>Heron Yang</h1>
+          <p>Student at Carnegie Mellon University</p>
+        </header>
+        <a href="#work" class="jumplink pic">
+          <span class="arrow icon fa-chevron-right"><span>See my work</span></span>
+          <img src="images/me.jpg" alt="" />
+        </a>
+      </article>
+
+      <!-- Work -->
+      <article id="work" class="panel">
+        <header>
+          <h2>Work</h2>
+        </header>
+
+        <section>
+          <b><a target="_blank" class="link" href="http://cmu.heron.me/">Heron at CMU</a></b>
+          <i>(latest)</i>
+        </section>
+
+        <h3>Study</h3>
+        <section>
+          <table>
+            <tr>
+              <td> <a target="_blank" class="link" href="http://heron-note.blogspot.tw/">Note</a> </td>
+              <td> my study notes on Computer Science topics </td>
+            </tr>
+            <tr>
+              <td> <a target="_blank" class="link" href="http://heron-article.blogspot.tw/ ">Article</a> </td>
+              <td> my own opinions and thinkings (Chinese) </td>
+            </tr>
+          </table>
+        </section>
+
+        <h3>Project</h3>
+        <section>
+          <table>
+            <tr>
+              <td><a target="_blank " class="link " href="http://lens-decider.herokuapp.com/ ">Lens Decider</a> </td>
+              <td>Blind Test for Lens Decision</td>
+            </tr>
+            <tr>
+              <td><a target="_blank " class="link " href="http://plate.tw/ ">PLATE</a> </td>
+              <td>Online Meal Ordering System</td>
+            </tr>
+            <tr>
+              <td><a target="_blank " class="link " href="http://www.pairs.cc/ ">PAIRS.cc</a> </td>
+              <td>Open Gossip Platform</td>
+            </tr>
+            <tr>
+              <td><a target="_blank " class="link " href="http://pitchit.nctucs.net/ ">Pitch it</a> </td>
+              <td>The Perfect Pitch Challenge (iOS App)</td>
+            </tr>
+            <tr>
+              <td> <a target="_blank " class="link " href="http://itrs.tw/wiki/Award-checking_System ">Award Checking System</a> </td>
+              <td> </td>
+            </tr>
+          </table>
+        </section>
+
+
+        <h3>Travel & Photography</h3>
+        <section>
+          <table>
+
+            <tr>
+              <td>
+                <a target="_blank " class="link " href="https://plus.google.com/photos/114740749970901093083/albums/6125738412175853809?authkey=CMn2n6Pu66HWVg">United States</a>
+              </td>
+              <td>
+                Orlando, Miami, Philadelphia - 2015 Spring
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <a target="_blank " class="link " href="https://plus.google.com/photos/+HeronYang/albums/6029964355378343505 ">China</a>
+              </td>
+              <td>
+                Macau - 2014 Summer
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <a target="_blank " class="link " href="http://heron-japan.blogspot.com/ ">Japan</a>
+              </td>
+              <td>
+                Osaka, Kyoto, Kobe - 2014 Winter
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <a target="_blank " class="link " href="http://heron-hawaii.blogspot.tw/ ">Hawaii</a>
+              </td>
+              <td>
+                2013 Summer
+              </td>
+            </tr>
+
+            <tr>
+              <td><a target="_blank " class="link " href="http://heron-england.blogspot.tw/ ">England</a>
+              </td>
+              <td>2012 Summer</td>
+            </tr>
+
+            <tr>
+              <td><a target="_blank " class="link " href="http://heron-stanford.blogspot.tw/ ">Stanford</a>
+              </td>
+              <td>San Fransicao, Los Angeles, Las Vegas, ... - 2011 Summer</td>
+            </tr>
+
+            <tr>
+              <td>
+                <a target="_blank " class="link " href="http://photography.heron.me/ ">Photography Collection</a>
+              </td>
+              <td></td>
+            </tr>
+          </table>
+        </section>
+
+        <h3><i>More</i></h3>
+        <section>
+          <table>
+            <tr>
+              <td> <a target="_blank " class="link " href="http://heronyang.github.io/calc/calculator.html ">Calculator</a> </td>
+              <td>Calculator (Practice)</td>
+            </tr>
+            <tr>
+              <td> <a target="_blank " class="link " href="http://heronyang.github.io/fbi ">Responsive FBI Page</a> </td>
+              <td>Frontend Design Practice (Responsive Website for FBI)</td>
+            </tr>
+            <tr>
+              <td> <a target="_blank " class="link " href="http://tap-in.me/ ">TAPIN</a> </td>
+              <td>Contact Sharing within a Single Tap </td>
+            </tr>
+            <tr>
+              <td><a target="_blank " class="link " href="http://cg.nctucs.net/ ">CG Creator</a> </td>
+              <td>Create Cinemagraph Animation by Uploading Videos</td>
+            </tr>
+            <tr>
+              <td> <a target="_blank " class="link " href="http://paper.nctucs.net/ ">PAPER</a> </td>
+              <td>Shared Whiteboard without Login </td>
+            </tr>
+
+            <tr>
+              <td> <a target="_blank " class="link " href="http://books-2u.appspot.com/ ">Books 2 U</a> </td>
+              <td>Second-hand Book Market </td>
+            </tr>
+
+            <tr>
+              <td> <a target="_blank " class="link " href="http://studio-zero.cc/ ">Studio Zero</a> </td>
+              <td>Landing Page for Studio Zero </td>
+            </tr>
+
+
+            <tr>
+              <td> <a target="_blank " class="link " href="http://typoo.nctucs.net/ ">Typoo</a> </td>
+              <td>Typing Test </td>
+            </tr>
+
+
+            <tr>
+              <td> <a target="_blank " class="link " href="http://life-timer.appspot.com/ ">Life Timer</a> </td>
+              <td>Countdown to Your Death </td>
+            </tr>
+
+
+            <tr>
+              <td> <a target="_blank " class="link " href="http://dmcc-2013.appspot.com/ ">DMCC 2013</a> </td>
+              <td>Annual Album from DMCC </td>
+            </tr>
+
+            <tr>
+              <td> <a target="_blank " class="link " href="http://pason-nosap.appspot.com/ ">Pason Nosap's Muzik Factory</a> </td>
+              <td>(Case) Personal Site for a SOHO Music Composer </td>
+            </tr>
+
+
+            <tr>
+              <td> <a target="_blank " class="link " href="http://www.spring.org.tw/ ">Spring Foundation of NCTU</a> </td>
+              <td>(Case) Official Website for Spring Foundation </td>
+            </tr>
+
+          </table>
+        </section>
+        <section>
+          <blockquote>
+            <span class="tab ">&nbsp;</span>The people who are crazy enough to think they can change the world, are the ones who do. <i>- the crazy ones</i>
+          </blockquote>
+        </section>
+      </article>
+
+
+      <!-- Contact -->
+      <article id="contact" class="panel">
+        <header>
+          <h2>Contact Me</h2>
+        </header>
+        <div class="row">
+          <div class="3u center-container">
+            <a target="_blank " href="mailto:heron.yang.tw@gmail.com "><i class="fa fa-envelope-o "></i></a>
+          </div>
+          <div class="3u center-container">
+            <a target="_blank " href="https://www.facebook.com/heron.yang "><i class="fa fa-facebook "></i></a>
+          </div>
+          <div class="3u center-container">
+            <a target="_blank " href="https://github.com/heronyang "><i class="fa fa-github "></i></a>
+          </div>
+          <div class="3u center-container">
+            <a target="_blank " href="https://www.linkedin.com/in/heronyang "> <i class="fa fa-linkedin "></i> </a>
+          </div>
+        </div>
+        <hr />
+        <blockquote>
+          Please let me know if you're interested in any of my work, or just want to say Hello!
+        </blockquote>
+      </article>
+
+    </div>
+
+    <!-- Footer -->
+    <div id="footer">
+      <ul class="copyright ">
+        <li>heron.yang.tw@gmail.com</li>
+        <li>Copyright &copy; 2015 Heron Yang.</li>
+      </ul>
+    </div>
+
+  </div>
+
+  <script>
+    var _gaq = [['_setAccount', 'UA-48898398-1'], ['_trackPageview']];
+    (function (d, t) {
+      var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+      g.src = 'http://www.google-analytics.com/ga.js';
+      s.parentNode.insertBefore(g, s)
+    }(document, 'script'));
+  </script>
+
+</body>
+
+</html>
